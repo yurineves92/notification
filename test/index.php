ed190584629e00cd8cb6ac8725e5b4c6470e852d
Yuri Neves <yuri.neves@quadritech.com.br>
2020-11-26 01:10:35 -0300

    update

--- test/index.php
+++ test/index.php
@@ -4,7 +4,7 @@ require __DIR__. '/../vendor/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email(2, "smtp.sendgrid.net", "secrret", "secret", "tls", 587, "yurineves92@gmail.com", "Yuri Neves");
+$novoEmail = new Email(2, "smtp.sendgrid.net", "secrret", "secret", "tls", "port", "yurineves92@gmail.com", "Yuri Neves");
 $novoEmail->sendEmail("Assunto de teste", "<h1>Esse é um <b>e-mail</b> de teste</h1>", "yuri.neves@quadritech.com.br", "Yuri", "yurineves92@gmail.com", "Yuri Neves");
 
 var_dump($novoEmail);
